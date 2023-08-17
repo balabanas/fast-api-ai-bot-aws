8a98b95099747d68cf2afb90b5c75a43098bcd97
Anton Balabanov <asbbnv@gmail.com>
2023-08-17 14:36:15 +0300

    core functionality

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# fast-api-ai-telegram-bot-aws
-AWS-deployed Fast API Telegram Bot with AI support
+# fast-api-ai-bot-aws
+AWS (Lambda)-deployed Fast API Bot with AI support.
+
+The project offers a full-fledged web application - a chat bot and an image-generation-bot, built on FastAPI, with integration with OpenAI ChatGPT and DALL-E models through OpenAI API. The project is intended to be deployed on AWS using Lambda. The project was made as part of a training course "The Complete Chatbot Bootcamp (OpenAI, GPT, FastAPI & AWS)" on Udemy (https://www.udemy.com/course/the-complete-chatbot-bootcamp).
+
+Oh, yes. You should know that chatbot initializes with a system prompt of "You are a Python developer", and as such, the responses might be appropriately biased :-)
+
+## On OpenAI API Key
+To run the app you'll need to create `.env` file in project's root (where `main.py` lives), and store there a key which authenticates you for OpenAI API services. See https://platform.openai.com/account/api-keys. Read instructions there carefully. Note, that you'll might need to pay for the usage, so keep the code safe!
+
+Like this:
+`OPENAI_API_KEY=<your key here>`
+
+
+## Run locally
+```uvicorn main:app --reload```
+
+
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+from typing import Annotated
+
+import openai
+from dotenv import load_dotenv
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from openai import InvalidRequestError
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+openai.api_key = OPENAI_API_KEY
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+chat_log: list = [{
+    'role': 'system',
+    'content': 'You are a Python AI tutor. Provide clear answers for a beginner software developers.'
+}]
+
+chat_responses: list = []
+image_url: str = ''
+
+
+@app.get('/', response_class=HTMLResponse)
+async def chat_page(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request, 'chat_responses': chat_responses[::-1]})
+
+
+@app.post('/', response_class=HTMLResponse)
+async def chat_response(request: Request, user_chat_input: Annotated[str, Form()]):
+    chat_log.append({'role': 'user', 'content': user_chat_input})
+    chat_responses.append(user_chat_input)
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=chat_log,
+        temperature=0.6
+    )
+    bot_response = response['choices'][0]['message']['content']
+    chat_log.append({'role': 'assistant', 'content': bot_response})
+    chat_responses.append(bot_response)
+    return templates.TemplateResponse('home.html', {'request': request, 'chat_responses': chat_responses[::-1]})
+
+
+@app.get('/image', response_class=HTMLResponse)
+async def image_page(request: Request):
+    return templates.TemplateResponse('image.html', {'request': request, 'image_url': image_url})
+
+
+@app.post('/image', response_class=HTMLResponse)
+async def image_response(request: Request, user_image_input: Annotated[str, Form()]):
+    global image_url
+    try:
+        response = openai.Image.create(
+            prompt=user_image_input,
+            n=1,
+            size="256x256"
+        )
+        image_url = response['data'][0]['url']
+        msg = ''
+    except InvalidRequestError as e:
+        msg = str(e)
+    return templates.TemplateResponse('image.html', {'request': request, 'image_url': image_url,
+                                                     'user_image_input': user_image_input, 'msg': msg})
